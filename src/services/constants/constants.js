e1752bb986f93a6a7dde5792620d3f249d34eb34
mahmudul1995 <mahmuduls451@gmail.com>
2022-07-04 15:29:12 +0600

    done

---
+++ src/services/constants/constants.js
@@ -0,0 +1,6 @@
+// - step 1: setup project & install packages `npm install redux react-redux redux-thunk axios`
+// - step 2: define constants-> src/services/constants/todosConstant.js
+
+export const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
+export const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
+export const GET_TODOS_FAILED = "GET_TODOS_FAILED";
